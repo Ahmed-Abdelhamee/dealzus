7b6eaea939e0b97f54d6dec4e77c306afbecc15e
Ahmed-Abdelhameed <medoabdelhameed99.yahoo.com>
2023-10-31 16:05:19 +0200

    frist commit

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -12,4 +12,35 @@ export class ClientsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  showImage:boolean=false;
+
+  image_Show_Link:string="";
+
+  clientsList:string[]=["assets/1.webp","assets/2.webp","assets/3.webp","assets/4.webp","assets/1.webp","assets/2.webp","assets/3.webp","assets/4.webp"]
+
+  show(item:string){
+    this.showImage=true;
+    this.image_Show_Link=item;
+  }
+
+  close(){
+    this.showImage=false;
+  }
+
+  left(item:string){
+    if( this.clientsList.indexOf(item) < this.clientsList.length - 1 ){
+      this.image_Show_Link = this.clientsList[this.clientsList.indexOf(item) + 1] ;
+    }
+    else
+      this.image_Show_Link = this.clientsList[0]
+  }
+
+  right(item:string){
+    if( this.clientsList.indexOf(item) == 0 ){
+      this.image_Show_Link = this.clientsList[this.clientsList.length-1] ;
+    }else{
+      this.image_Show_Link = this.clientsList[this.clientsList.indexOf(item) - 1] ;
+    }
+  }
+  
 }
